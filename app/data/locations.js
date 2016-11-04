2e344336fb3b1946c6085928157e67d1ede45341
K3nnythe13ed <stvdiab@aol.com>
2016-11-04 10:42:35 +0100

    location

---
+++ app/data/locations.js
@@ -0,0 +1,97 @@
+var location1 = {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  
+       [53.543712,
+           9.988552]
+    },
+    "properties": {
+    "LocID":"1",
+    "OE":"DE",
+    "AccountName":"Test 1",
+    "Entire":", DE,  0,  0,  0",
+    "Exp_TIV":"1000000",
+    "ML_AGCS_Share":"",
+    "MR_RISK_SCORE":"4",
+    "AddrMatch":"",
+    "AAL_PreCat_EQ":"",
+    "AAL_PreCat_FL":"",
+    "AAL_PreCat_WS":""
+    }
+  };
+  
+  var location2 = {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [40.707517,
+       -74.009413
+           
+       ]
+    },
+    "properties": {
+    "LocID":"2",
+    "OE":"US",
+    "AccountName":"Test 2",
+    "Entire":", US,  0,  0,  0",
+    "Exp_TIV":"5000000",
+    "ML_AGCS_Share":"",
+    "MR_RISK_SCORE":"3",
+    "AddrMatch":"",
+    "AAL_PreCat_EQ":"",
+    "AAL_PreCat_FL":"",
+    "AAL_PreCat_WS":""
+    }
+  };
+
+  var location3 = {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [
+           39.013322,
+           117.802829
+       ]
+    },
+    "properties": {
+    "LocID":"3",
+    "OE":"DE",
+    "AccountName":"Test 3",
+    "Entire":", CN,  0,  0,  0",
+    "Exp_TIV":"7500000",
+    "ML_AGCS_Share":"",
+    "MR_RISK_SCORE":"4",
+    "AddrMatch":"",
+    "AAL_PreCat_EQ":"",
+    "AAL_PreCat_FL":"",
+    "AAL_PreCat_WS":""
+    }
+  };
+
+var location4 = {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [
+           51.892873,
+           4.449863
+           
+       ]
+    },
+    "properties": {
+    "LocID":"4",
+    "OE":"DE",
+    "AccountName":"Test 4",
+    "Entire":", NL,  0,  0,  0",
+    "Exp_TIV":"10000000",
+    "ML_AGCS_Share":"",
+    "MR_RISK_SCORE":"4",
+    "AddrMatch":"",
+    "AAL_PreCat_EQ":"",
+    "AAL_PreCat_FL":"",
+    "AAL_PreCat_WS":""
+    }
+  };
+  var demoLocations = [location1, location2, location3, location4];
+
