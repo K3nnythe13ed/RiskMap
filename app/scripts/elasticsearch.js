16da7d95bf034bbf67971d283e26a381fa1b1c67
K3nnythe13ed <stvdiab@aol.com>
2016-11-15 12:25:49 +0100

    search 50 results

--- app/scripts/elasticsearch.js
+++ app/scripts/elasticsearch.js
@@ -4,23 +4,7 @@ var allTitles = [];
 client.search({
   index: 'ais-2016.11.09',
   type: 'vessel',
-  // Set to 30 seconds because we are calling right back
-  scroll: '30s',
-  storedFields: ['MMSI'],
-  q: 'MMSI:244630536'
-}, function getMoreUntilDone(error, response) {
-  // collect the title from each response
-  response.hits.hits.forEach(function (hit) {
-    allTitles.push(hit.storedFields);
-  });
-
-  if (response.hits.total !== allTitles.length) {
-    // now we can call scroll over and over
-    client.scroll({
-      scrollId: response._scroll_id,
-      scroll: '30s'
-    }, getMoreUntilDone);
-  } else {
-    console.log('every "test" title', allTitles);
-  }
+  size: '50'
+}, function (error, response) {
+  // ...
 });
