77e550bf022b61a4c2a953f0ad66688d7a96d15b
K3nnythe13ed <stvdiab@aol.com>
2016-11-15 15:07:26 +0100

    search all vessels

--- app/scripts/elasticsearch.js
+++ app/scripts/elasticsearch.js
@@ -1,10 +1,29 @@
 var allTitles = [];
 
-// first we do a search, and specify a scroll timeout
-client.search({
-  index: 'ais-2016.11.09',
-  type: 'vessel',
-  size: '50'
-}, function (error, response) {
-  // ...
-});
+function getAllVessels()
+{
+            client.search({
+                index: 'ais-2016.11.09' ,
+                type: 'vessel',
+                size: 5
+            }, function(err, resp, _respcode) {
+               
+                const documents = [];
+                for (let i = 0; i < resp.hits.hits.length; i++) {
+                    
+                    documents[documents.length] = {
+                        _index: resp.hits.hits[i]._index,
+                        _type: resp.hits.hits[i]._type,
+                        _id: resp.hits.hits[i]._id,
+                        MMSI: resp.hits.hits[i]._source.MMSI,
+                        LOCATION: resp.hits.hits[i]._source.LOCATION
+                        
+                    };
+                    console.log(documents)
+                }
+            });
+        
+}
+$(function(){
+   var doc = getAllVessels()
+})
