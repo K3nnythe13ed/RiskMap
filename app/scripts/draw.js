d4451254fce82daec263ce7bc01f9f646db87b3c
K3nnythe13ed <stvdiab@aol.com>
2016-11-17 11:51:26 +0100

    adding edit function changing values in table

--- app/scripts/draw.js
+++ app/scripts/draw.js
@@ -1,7 +1,7 @@
 
 
 var editableLayers = new L.FeatureGroup();
-    map.addLayer(editableLayers);
+map.addLayer(editableLayers);
 var layer;
 var options = {
     edit: {
@@ -20,25 +20,39 @@ var drawControl = new L.Control.Draw(options);
 map.addControl(drawControl);
 
 map.on(L.Draw.Event.DRAWSTART, function (e) {
-    
+
     editableLayers.clearLayers();
-   
+
 })
 map.on(L.Draw.Event.CREATED, function (e) {
-  
-        
-        var type = e.layerType,
+
+    console.log(e)
+    var type = e.layerType,
         layer = e.layer;
-        
     //on create do elasticsearch function countVessels input(function replaceTableValue as callback, layer latlongs)
     countVessels(replaceTableValue, getAllVessels, layer.getLatLngs())
 
     editableLayers.addLayer(layer);
-    
+
     // Do whatever else you need to. (save to db, add to map etc)
 });
 
-    map.on(L.Draw.Event.DELETESTART, function (e) {
-      editableLayers.clearLayers();
-    });
+map.on(L.Draw.Event.DELETESTART, function (e) {
+    editableLayers.clearLayers();
+    var latlong = undefined
+    countVessels(replaceTableValue, getAllVessels, latlong)
+});
+
+map.on(L.Draw.Event.EDITED, function (e) {
+    console.log(e);
+    i=0;
+    while(e.layers._layers[i] === undefined)
+    {
+        i++;
+    }
+    layer = e.layers._layers[i]._latlngs;
+        countVessels(replaceTableValue, getAllVessels, layer)
+
+
+});
 // Initialise the draw control and pass it the FeatureGroup of editable layers
